fc1743c00800be19ba1a259dc08d7f35dbc74d18
gzmymn <77552353+gzmymn@users.noreply.github.com>
2021-05-05 17:59:00 +0300

    day4_homework3

--- day4_Homework3_GameProject/src/day4_Homework3/Main.java
+++ day4_Homework3_GameProject/src/day4_Homework3/Main.java
@@ -14,51 +14,48 @@ import day4_Homework3.entities.Gamer;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		//OYUN OLUÞTURMA
+
+		// OYUN OLUSTURMA
 		Game game1 = new Game(1, "Sky Soldier", "War", 300);
-		
+
 		GameManager gameManager = new GameManager();
-		//OYUN EKLEME
+		// OYUN EKLEME
 		gameManager.add(game1);
-		//OYUN SÝLME
+		// OYUN SILME
 		gameManager.delete(game1);
-		//OYUN GÜNCELLEME
+		// OYUN GUNCELLEME
 		gameManager.update(game1);
-		
-		
-		
-		//OYUNCU OLUÞTURMA
+
+		// OYUNCU OLUSTURMA
 		Gamer gamer1 = new Gamer(1, "Gizem", "Yaman", "1234567", LocalDate.of(1995, 5, 5), "gzm");
-		
+
 		GamerManager gamerManager = new GamerManager(new MernisServiceAdapter());
-		
-		//OYUNCU EKLEME
+
+		// OYUNCU EKLEME
 		gamerManager.add(gamer1);
-		//OYUNCU SÝLME
+		// OYUNCU SILME
 		gameManager.delete(game1);
-		//OYUNCU GÜNCELLEME
+		// OYUNCU GUNCELLEME
 		gameManager.update(game1);
-		
-		//KAMPANYA OLUÞTURMA
+
+		// KAMPANYA OLUSTURMA
 		Campaign campaign1 = new Campaign(1, "May opportunity", 50);
-		
+
 		CampaignManager campaignManager = new CampaignManager();
-		
-		//KAMPANYA EKLEME
+
+		// KAMPANYA EKLEME
 		campaignManager.add(campaign1, game1);
-		//KAMPANYA SÝLME
+		// KAMPANYA SILME
 		campaignManager.delete(campaign1, game1);
-		//KAMPANYA GÜNCELLEME
+		// KAMPANYA GUNCELLEME
 		campaignManager.update(campaign1, game1);
 
-		
-		//SATIÞ ÝÞLEMLERÝ
-        SalesManager salesManager = new SalesManager();
-        //STANDART SATIÞ
-        salesManager.StandartSales(game1, gamer1);
-        //KAMPANYALI SATIÞ
-        salesManager.CampaignSales(game1, gamer1, campaign1);
+		// SATIÞ ÝSLEMLERÝ
+		SalesManager salesManager = new SalesManager();
+		// STANDART SATIS
+		salesManager.StandartSales(game1, gamer1);
+		// KAMPANYALI SATIS
+		salesManager.CampaignSales(game1, gamer1, campaign1);
 	}
 
 }
